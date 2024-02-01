7b8db610f7260ca4bf123f668b1e424509f43dd4
Eduardo Lorenzo <ejlorenzo19@gmail.com>
2024-02-01 13:42:53 +0100

    Trying to fix bin

--- bin/ecommit.js
+++ bin/ecommit.js
@@ -6,6 +6,7 @@
 
 var cli = require(__dirname + '/../index.cjs');
 if (!cli.autoRun) {
+  console.log(cli)
   cli.default().catch(function(error) {
     console.error(error.stack || error.message || error);
     process.exitCode = 1;
